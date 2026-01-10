dd0e46cd0807de2b62acc4ed10bfb53834650db1
Artem Ushenko <99095504+Artem-Ushenko@users.noreply.github.com>
2026-01-10 17:02:24 +0200

    Add sales tab barcode handling

---
+++ internal/store/models.go
@@ -0,0 +1,16 @@
+package store
+
+type Customer struct {
+	ID    int64
+	Name  string
+	Email string
+	Phone string
+}
+
+type Product struct {
+	ID      int64
+	Name    string
+	Barcode string
+	Price   float64
+	Stock   int64
+}
